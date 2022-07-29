c5211454d2db25524cd749d0e4b2b97ac5ee4c7e
Blake Smith <blake.smith.mn@gmail.com>
2022-07-28 19:17:12 -0500

    wrapped my brain around basic for loops and answered some questions

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -6,25 +6,44 @@ console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
 console.log('count from 0 to 3');
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-for (let i=0; i<4; i++) {  
+for (let i = 0; i < 4; i++) {
   console.log(i);
 }
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+for (let i = 0; i <= 5; i++) {
+  console.log(i);
+}
+// we would change the value at which our loop stops - in the middle section of the for loop
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+for (let i = 3; i <= 5; i++) {
+  console.log(i);
+}
+
+// changing starting value for the loop, as well as where the loop is checking to see if the code will run
+
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+for (let i = 2; i <= 10; i += 2) {
+  console.log(i);
+}
+
+//in this example we are changing the start and end of the loop, as well as the additive property
+// this lets us print only the evens!
+
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for (i = 5; i >= 0; i--) {
+  console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
