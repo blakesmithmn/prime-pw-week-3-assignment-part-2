d3c0d370be352f8483b7d000e4cd0c4baadaaf72
Blake Smith <blake.smith.mn@gmail.com>
2022-08-03 18:23:06 -0500

    after getting feedback on part 3 I added 'let' to some loops in this assignment

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -42,7 +42,7 @@ for (let i = 2; i <= 10; i += 2) {
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-for (i = 5; i >= 0; i--) {
+for (let i = 5; i >= 0; i--) {
   console.log(i);
 }
 // wow look at us doing basic counting 
